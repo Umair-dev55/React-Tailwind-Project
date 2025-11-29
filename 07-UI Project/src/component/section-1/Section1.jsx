95694c784d8aba14bbd01718f4d04ada87f3a14d
umair-dev55 <umair.personal55@gmail.com>
2025-11-29 23:46:37 +0500

    bug fix

--- 07-UI Project/src/component/section-1/Section1.jsx
+++ 07-UI Project/src/component/section-1/Section1.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Navbar from "./Navbar";
 import PageContent from "./PageContent";
@@ -13,14 +12,3 @@ const Section1 = () => {
 };
 
 export default Section1;
-=======
-import React from 'react'
-
-const Section1 = () => {
-  return (
-    <div className='h-screen bg-blue-950'>Section-1</div>
-  )
-}
-
-export default Section-1
->>>>>>> 786c57a07bbbc143e212dfc8c1be3e3b926a72ec
--- 07-UI Project/src/component/section-2/Section2.jsx
+++ 07-UI Project/src/component/section-2/Section2.jsx
@@ -1,13 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Section2 = () => {
-  return (
-<<<<<<< HEAD
-    <div className='bg-red-950 text-white h-screen '>Section2</div>
-=======
-    <div>Section2</div>
->>>>>>> 786c57a07bbbc143e212dfc8c1be3e3b926a72ec
-  )
-}
+  return <div className="bg-red-950 text-white h-screen">Section2</div>;
+};
 
-export default Section2
+export default Section2;
