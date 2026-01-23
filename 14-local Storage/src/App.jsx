a58ffe6c5882cd413e1156cea4d4b5a3bd64139e
umair-dev55 <umair.personal55@gmail.com>
2026-01-24 00:17:18 +0500

    localstorage done

--- 14-local Storage/src/App.jsx
+++ 14-local Storage/src/App.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React from 'react'
 
 const App = () => {
-  // localStorage.setItem("age", "20");
-  let age = localStorage.getItem("age");
-  console.log(age);
+  let user = JSON.parse(localStorage.getItem('user'));
+  console.log(user);
 
-  localStorage.clear;
-  return <div>App</div>;
-};
 
-export default App;
+  let student = {
+    name: 'umair',
+    Semmester: '4th semmester',
+    CGPA: 3.1
+  }
+  console.log(student);
+
+  // JSON.stringify store array 7 object in localstorage in string form
+  localStorage.setItem('student', JSON.stringify(student))
+  // JSON.parse use for return in orignal type
+  let students = JSON.parse(localStorage.getItem('student'));
+  console.log(students);
+  // set in localStorage
+  // localStorage.setItem
+
+  // get from localStorage
+  // localStorage.getItem
+
+  // for remove
+  // localStorage.removeItem
+
+  // for clear localStorage
+  // localStorage.clear
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
