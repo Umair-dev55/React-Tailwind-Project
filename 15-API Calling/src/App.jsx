8f22e96b1480b891062ee1b6a07da71a66658082
umair-dev55 <umair.personal55@gmail.com>
2026-01-25 14:09:25 +0500

    api calling done

--- 15-API Calling/src/App.jsx
+++ 15-API Calling/src/App.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
+  const [data, setdata] = useState([])
   let getdata = async () => {
-    let api = await fetch('https://jsonplaceholder.typicode.com/photos')
+
+    let { data } = await axios.get('https://picsum.photos/v2/list')
+    setdata(data);
+    console.log(data);
+
+
+
+    //-------- using axios 3rd party api fetch--------
+
+    // let response = await axios.get('https://jsonplaceholder.typicode.com/photos')
+    // console.log(response.data);
+
+
+
+    // --------Fetch Rest Api --------
+
+    // let api = await fetch('https://jsonplaceholder.typicode.com/photos')
     // console.log(api);
-    console.log(await (api.json()));
+    // console.log(await (api.json()));
 
   }
   return (
     <div>
       <button onClick={getdata}>Get Data</button>
+
+      {data.map((elem, idx) => {
+        return <h3>history{elem.author}{idx}</h3>;
+      })}
     </div>
   )
 }
